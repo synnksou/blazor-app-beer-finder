06c9a236000e4a7d9ec968712ca5aa0bda54c580
Heinrich Antoine <antoine.h@tymate.com>
2021-10-05 12:19:38 +0200

    first commit

---
+++ Data/BeerData.cs
@@ -0,0 +1,12 @@
+/* namespace testblazor.Data {
+    class Beer
+    {
+        public String name {get; set;}
+        public String tagline {get; set;}
+        public String type {get; set;}
+        public String description {get; set;}
+        public Date date {get; set;}
+        public String abv{get; set;}
+    }
+}
+ */
